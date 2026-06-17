4bf02f8fd8703965d575548b0bc812c9a2bf558f
phatnguyen <phatnguyentan.aiengineer@gmail.com>
2026-06-17 09:08:06 +0700

    Experiment 4

--- shell/run-custom-HS0010.sh
+++ shell/run-custom-HS0010.sh
@@ -15,7 +15,7 @@ python main.py \
     --pretrain_embed_dimension 1536 \
     --target_embed_dimension 1536 \
     --patchsize 3 \
-    --meta_epochs 100 \
+    --meta_epochs 120 \
     --eval_epochs 1 \
     --dsc_layers 2 \
     --dsc_hidden 1024 \
